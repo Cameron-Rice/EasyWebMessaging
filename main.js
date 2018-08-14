7c9d360660c69f632d2df1cf7330aff8ca75ecec
cameron <rice0061@algonquinlive.com>
2018-08-14 19:49:03 -0400

    Added basic Tray functionality. Minimize to Tray.

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
-const {app, BrowserWindow} = require('electron')
+const {app, Tray, Menu, BrowserWindow} = require('electron')
+const path = require('path')
 
-let win;
+const iconPath = path.join(__dirname, "assets/icons/icon-64.png")
+
+let tray = null;
+let win = null;
+
+let closeToTray = false;
 
 function bootApp () {
 
 		win = new BrowserWindow({
 			height: 1200,
 			width: 720,
-			titleBarStyle: "hiddenInset",
-			Show: false
+			minHeight: 400,
+			minWidth: 400, 
+			titleBarStyle: "hidden",
+			Show: false,
+			title: 'Easy Web Messaging',
+			icon: iconPath
 		});
 
 		win.setMenu(null);
 
 		win.loadURL("https://messages.android.com/");
 
-		win.on('ready-to-show', () => {
-			win.Show();
-		});
+		registerWindowEventListeners();
 
-		win.on('closed', () => {
-			win = null;
-		});
+		tray = new Tray(iconPath);
+
+		tray.setToolTip("Easy Web Messaging!")
+
+		var contextMenu = Menu.buildFromTemplate([
+		{
+			label: 'Close',
+			click() {app.quit()}
+		}
+		]);
+
+		tray.setContextMenu(contextMenu);
+
+		registerTrayEventListeners();
 }
 
 app.on('ready', bootApp);
@@ -37,3 +56,33 @@ app.on('activate', () => {
 	  createWindow()
 	}
 });
+
+function registerWindowEventListeners(){
+	win.on('ready-to-show', () => {
+		win.Show();
+	});
+
+	win.on('closed', () => {
+		win = null;
+	});
+
+	win.on('page-title-updated', (e) =>{
+		e.preventDefault();
+	});
+
+	win.on('minimize', (e) => {
+		e.preventDefault();
+		closeToTray = true;
+		win.hide();
+	});
+
+}
+
+function registerTrayEventListeners(){
+	tray.on('double-click', () => {
+		if(closeToTray = true){
+			win.show();
+			closeToTray = false;
+		}
+	});
+}
